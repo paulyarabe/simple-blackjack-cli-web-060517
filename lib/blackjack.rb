d38e4bed311977889f5f6d6187fe25b65b6cba1e
Paul Yarabe <pyarabe@mba2018.hbs.edu>
2017-05-27 18:05:35 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,60 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(current_card_total)
+  prompt_user
+  user_input = get_user_input
+  if user_input == 's'
+    current_card_total
+  elsif user_input == 'h'
+    current_card_total += deal_card
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command."
 end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
+require 'pry'
 
 def runner
-  # code runner here
+  welcome
+  cardsum = initial_round
+  until cardsum > 21
+    cardsum = hit?(cardsum)
+    display_card_total(cardsum)
+  end
+  end_game(cardsum)
 end
-    
